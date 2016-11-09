c66aa4a572b9412e83b3cbf0334338efcade7ba0
CJAllen056 <christopherjallen056@gmail.com>
2016-11-09 18:07:15 +0000

    added config and required in app along with node modules.

---
+++ hackajob-phone-book/app.js
@@ -0,0 +1,7 @@
+var express         = require("express");
+var morgan          = require("morgan");
+var methodOverride  = require("method-override");
+var bodyParser      = require("body-parser");
+var mongoose        = require("mongoose");
+
+var config          = require("./config/config.js");
---
+++ hackajob-phone-book/config/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  port:   process.env.PORT || 3000,
+  database: process.env.MONGODB_URI || "mongodb://localhost/hackajobPhonebook"
+};
