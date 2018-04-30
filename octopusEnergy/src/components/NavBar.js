55b77e5468b67bc4f46e84d165dc17f7396c32ab
CJAllen056 <christopherjallen056@gmail.com>
2018-04-30 21:00:38 +0100

    styled nav bar

--- octopusEnergy/src/components/NavBar.js
+++ octopusEnergy/src/components/NavBar.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
-import OctopusLogo from '../assets/octopus-energy-logo.png'
+import OctopusLogo from '../assets/octopus-energy-logo.svg'
 
 class NavBar extends React.Component {
   render () {
     return (
-      <div>
-        <img src={OctopusLogo} />
+      <div className='nav-bar'>
+        <div className='logo-box'>
+          <img src={OctopusLogo} />
+        </div>
         <i className='material-icons'>menu</i>
       </div>
     )
